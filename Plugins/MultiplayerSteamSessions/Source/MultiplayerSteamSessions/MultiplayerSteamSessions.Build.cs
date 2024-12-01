5250dda07adecc8f1a903468566782914b631bed
TanushKrashak <tankrashak38816@gmail.com>
2024-12-01 20:56:35 +0530

    Added Required dependencies for that plugin

--- Plugins/MultiplayerSteamSessions/Source/MultiplayerSteamSessions/MultiplayerSteamSessions.Build.cs
+++ Plugins/MultiplayerSteamSessions/Source/MultiplayerSteamSessions/MultiplayerSteamSessions.Build.cs
@@ -27,6 +27,8 @@ public class MultiplayerSteamSessions : ModuleRules
 			{
 				"Core",
 				// ... add other public dependencies that you statically link with here ...
+				"OnlineSubsystem",
+				"OnlineSubsystemSteam",
 			}
 			);
 			
