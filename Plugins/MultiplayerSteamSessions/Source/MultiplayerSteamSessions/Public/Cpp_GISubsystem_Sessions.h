7b9aeae49ae3a8c8e5d266aeea24644b11a7f2d0
TanushKrashak <tankrashak38816@gmail.com>
2024-12-01 21:18:35 +0530

    created the plugin again, this time its working

---
+++ Plugins/MultiplayerSteamSessions/Source/MultiplayerSteamSessions/Public/Cpp_GISubsystem_Sessions.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "Cpp_GISubsystem_Sessions.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MULTIPLAYERSTEAMSESSIONS_API UCpp_GISubsystem_Sessions : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+	
+};
