24ac0174bbecb074ca6f4f299e75e176ae91cd5f
TanushKrashak <tankrashak38816@gmail.com>
2024-12-03 11:27:06 +0530

    Button functions are being called

--- Plugins/MultiplayerSteamSessions/Source/MultiplayerSteamSessions/Private/Cpp_WGT_Menu.cpp
+++ Plugins/MultiplayerSteamSessions/Source/MultiplayerSteamSessions/Private/Cpp_WGT_Menu.cpp
@@ -2,12 +2,13 @@
 
 
 #include "Cpp_WGT_Menu.h"
+#include "Components/Button.h"
 
 void UCpp_WGT_Menu::InitializeMenu() {
 	// Add the widget to the viewport & set it to visible & focusable
 	AddToViewport();
 	SetVisibility(ESlateVisibility::Visible);
-	bIsFocusable = true;
+	SetIsFocusable(true);
 
 	// Show the mouse cursor & set the input mode to UI only
 	if (UWorld* World = GetWorld()) {
@@ -20,4 +21,25 @@ void UCpp_WGT_Menu::InitializeMenu() {
 		}
 	}
 	
+}
+bool UCpp_WGT_Menu::Initialize() {
+	if (!Super::Initialize()) {
+		return false;
+	}
+	
+	// Bind the buttons to their respective OnClicked functions
+	if (BTN_Host) {
+		BTN_Host->OnClicked.AddDynamic(this, &UCpp_WGT_Menu::OnHostClicked);
+	}
+	if (BTN_Join) {
+		BTN_Join->OnClicked.AddDynamic(this, &UCpp_WGT_Menu::OnJoinClicked);
+	}
+	return true;
+}
+
+void UCpp_WGT_Menu::OnHostClicked() {
+	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Green, TEXT("Host Clicked!"));
+}
+void UCpp_WGT_Menu::OnJoinClicked() {
+	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Green, TEXT("Join Clicked!"));
 }
